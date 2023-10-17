4eea238492575ee8c631c1162f17195d5113d692
DevanshuLilhore <115913241+DevanshuLilhore@users.noreply.github.com>
2023-10-18 03:39:43 +0530

    Update Competitive.py

--- Competitive.py
+++ Competitive.py
@@ -1,4 +1,4 @@
-'''
+# factorial
 def factorial(n):
     if n==0:
         return 1
@@ -7,6 +7,7 @@ n=int(input("Enter a number :"))
 x=factorial(n)
 print("factorial of ",n," :",x)
 
+# Area of circle
 def area(n):
     d=n/2
     a=3.14*d*d
@@ -15,6 +16,7 @@ n=int(input("Enter diametar of circle :"))
 x=area(n)
 print("area of circle :",x)
 
+# HCF findout
 def GCD(n1,n2):
     hcf=1
     k=n1 if n1<n2 else n2
@@ -28,18 +30,21 @@ n2=int(input("Enter second number:"))
 x=GCD(n1,n2)
 print("GCD :",x)
 
+# Cheak prime number
+import math
 def prime(n):
     c=0
     for i in range(1,n+1):
         if(n%i==0):
             c=c+1
     if c==2:
-        print("prime number :",n,"Square root : ",n/2)
+        print("prime number :",n,"Square root : ",math.sqrt(n))
     else:
         print("not prime number :",n)
 n=int(input("Enter a number : "))
 prime(n)
 
+# Print prime number between interval
 def prime(n1,n2):
     for n1 in range(n1,n2+1):
         c=0
@@ -52,6 +57,8 @@ n1=int(input("Enter num1 number : "))
 n2=int(input("Enter num2 number : "))
 prime(n1,n2)
 
+
+# Sum of between interval prime number
 def prime(n1,n2):
     sum=0
     for n1 in range(n1,n2+1):
@@ -67,6 +74,8 @@ n1=int(input("Enter num1 number : "))
 n2=int(input("Enter num2 number : "))
 prime(n1,n2)
 
+
+# Find largest number between three number
 def Great(n1,n2,n3):
     print("Greatest number :",end="")
     if n1>n2 and n1>n3:
@@ -78,34 +87,31 @@ def Great(n1,n2,n3):
             print(n3)
 Great(5,3,5)
 
-def Great(n1,n2,n3):
-    print("Smallest number :",end="")
-    if n1<n2 and n1<n3:
-        print(n1)
+# Find smallest number between four number
+n1=int(input("Enter a value :"))
+n2=int(input("Enter b value :"))
+n3=int(input("Enter c value :"))
+n4=int(input("Enter c value :"))
+if n1<n2:
+    if n1<n3:
+        if n1<n4:
+            print("Smallest number :",n1)
+        else:
+            print("Smallest number :",n4)
     else:
-        if n2<n3:
-            print(n2)
+        if n3<n4:
+            print("Smallest number :",n3)
         else:
-            print(n3)
-Great(5,3,5)
-
-def Great(n1,n2,n3):
-    print("Smallest number :",end="")
-    if n1>=n2 and n1>=n3:
-        if(n2>=n3):
-            print(n2)
+            print("Smallest number :",n4)
+else:
+    if n2<n3:
+        if n2<n4:
+            print("Smallest number :",n2)
         else:
-            print(n3)
+            print("Smallest number :",n4)
     else:
-        if n2>=n1 and n2>=n3:
-            if(n1>=n3):
-                print(n1)
-            else:
-                print(n3)
+        if n3<n4:
+            print("Smallest number :",n3)
         else:
-            if(n1>=n2):
-                print(n1)
-            else:
-                print(n2)
-Great(5,3,4)
-'''
+            print("Smallest number :",n4)
+    
